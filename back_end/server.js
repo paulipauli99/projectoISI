bc31c3e378cfd0ee7eb9db0f5011b6bc469028b0
Andre-isi <61022830+Andre-isi@users.noreply.github.com>
2020-04-18 20:05:29 +0100

    upd

--- back_end/server.js
+++ back_end/server.js
@@ -6,12 +6,10 @@ app.use(bodyParser.json());
 
 app.use(express.static('../front_end'));
 
-//app.use('/jasmin', require('./routes/jasmin'));
-//app.use('/hubspot', require('./hubspot.js'));
-
 const PORT = process.env.PORT || 8080;
-
 app.listen(PORT, () => console.log("Running on port: " + PORT));
+
 module.exports = { app };
+
 require('./routes/artigosroutes.js');
-//require('./jasmincontroller.js');
+require('./routes/clientesroutes.js');
