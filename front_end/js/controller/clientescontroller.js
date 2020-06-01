4bdf2f1fee99f46c1a304ec03c5037d39ab2ef9a
Sara Mendes <43757696+sluisam@users.noreply.github.com>
2020-06-01 12:40:59 +0100

    Revert "x"
    This reverts commit f071db7d29d7d16933edf29fc026e6f42b56b186.

--- front_end/js/controller/clientescontroller.js
+++ front_end/js/controller/clientescontroller.js
@@ -47,8 +47,8 @@ function tabela_clientes(clientes) {
             txt += "<td>" + cliente.distrito + "</td>";
             txt += "<td>" + cliente.codigo_postal + "</td>";
 
-            txt += "<td> <form action='/editarAdmin' method='post'> <input type='hidden' id='editarID' name='editarID' value=" + idCliente + "></input> <button class='btn btn-info btn-lg'>Editar</button> </form> </td>";
-            txt += "<td> <form action='/apagarCliente' method='post'> <input type='hidden' id='apagarID' name='apagarID' value=" + idCliente + "></input> <button class='btn btn-info btn-lg'>Apagar</button> </form> </td>";
+            txt += "<td> <form action='/editarAdmin' method='post'> <input type='hidden' id='editarID' name='editarID' value=" + idCliente + "></input> <button>Editar</button> </form> </td>";
+            txt += "<td> <form action='/apagarCliente' method='post'> <input type='hidden' id='apagarID' name='apagarID' value=" + idCliente + "></input> <button>Apagar</button> </form> </td>";
             txt += "</tr>";
         }
     }
