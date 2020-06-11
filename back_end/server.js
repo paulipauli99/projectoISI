f7a2ee796c3e8ab31f08c8f4994522ecebdf0dba
Andre-isi <61022830+Andre-isi@users.noreply.github.com>
2020-06-12 00:11:07 +0100

    finalizado???

--- back_end/server.js
+++ back_end/server.js
@@ -6,35 +6,27 @@ const querystring = require("querystring");
 const rp = require('request-promise');
 const session = require('express-session');
 const multer = require('multer');
-
 const path = require('path');
 
+
 const storage = multer.diskStorage({
-   destination: '../front_end/upload/',
-   filename: function(req,file,cb){
-     console.log('storage');
-     console.log(file.originalname);
-     cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-   }
-
+    destination: '../front_end/upload/',
+    filename: function(req, file, cb) {
+        console.log('storage');
+        console.log(file.originalname);
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
 });
-
 const upload = multer({
     storage: storage,
-    limits:{fileSize: 5000000}
-
+    limits: { fileSize: 5000000 }
 }).single('myImage');
-
-
-
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('../front_end'));
 app.use(cors());
-
-
 app.use(session({
     name: 'sid',
     saveUninitialized: false,
@@ -47,10 +39,11 @@ app.use(session({
     }
 }));
 
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log("Running on: http://localhost:" + PORT + "/"));
 
-module.exports = { app,upload };
+module.exports = { app, upload };
 
 require('./routes/artigosroutes.js');
 require('./routes/clientesroutes.js');
